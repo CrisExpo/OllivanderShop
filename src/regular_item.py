e723802784bb2c82aeea79935a44cff89f243d87
CrisExpositoSerra <crisexposito18@gmail.com>
2020-01-08 13:25:01 +0100

    feat(regular_item.py): added regular_item and test
    added Regular_item (sell_in, update_quality, check_quality, update_item,
    updated_item)
    Added pytest file:
    2 test executed, 2 test passed

---
+++ src/regular_item.py
@@ -0,0 +1,35 @@
+from item import Item
+
+
+class RegularItem(Item):
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def sell_in(self):
+        self.sell_in -= 1
+        return self.sell_in
+
+    def update_quality(self):
+        if self.sell_in > 0:
+            self.quality -= 1
+        else:
+            self.quality -= 2
+        return self.quality
+
+    def check_quality(self):
+        if self.quality < 0:
+            self.quality = 0
+        if self.quality > 50:
+            self.quality = 50
+
+    def update_item(self):
+        RegularItem.sell_in(self)
+        RegularItem.update_quality(self)
+        RegularItem.check_quality(self)
+        
+    def updated_item(self):
+        return self.name, self.sell_in, self.quality
+
+
