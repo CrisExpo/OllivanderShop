41d871ec1bac8f5dfec09784887a7c025cc9b7ff
CrisExpositoSerra <crisexposito18@gmail.com>
2020-01-08 16:48:24 +0100

    feat(conjured.py ): added conjured and test
    added conjured (update_quality, and update_item)
    added pytest
    file:
    3 test executed, 3 text passed
    
    TODO - sulfuras update quality is wrong

---
+++ src/conjured.py
@@ -0,0 +1,18 @@
+from regular_item import RegularItem
+
+
+class Conjured(RegularItem):
+    
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+     
+    def update_quality(self):
+        self.quality -=2
+        
+    def update_item(self):
+        RegularItem.sell_in(self)
+        Conjured.update_quality(self)
+        RegularItem.check_quality(self)   
+    
