f0ba30b215efb3767306c61de8107deef5b0b689
CrisExpositoSerra <crisexposito18@gmail.com>
2020-01-09 14:31:15 +0100

    feature(index.html): added basic front
    added basic front
    TODO: show list stock on web

--- src/app.py
+++ src/app.py
@@ -1,24 +1,17 @@
 from flask import Flask, render_template
+from gilded_rose import GildedRose
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
 
-@app.route('/')
-def index():
-    return "<h1>hello world!</h1>"
 
-@app.route('/blog')
+@app.route('/')
 def blog():
     return render_template('index.html')
 
-@app.route('/blog/<blog_id>')
-def blogpost(blog_id):
-    return "this is a Blog number " + str(blog_id)
 
-@app.route('/about')
-def about():
-    return "<h1>Know more about us</h1>"
+
 
 
 if '__name__' == '__main__':
