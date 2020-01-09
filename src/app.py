bca347a069a44da090baeea618655f00d6bf99fa
CrisExpositoSerra <crisexposito18@gmail.com>
2020-01-09 11:04:44 +0100

    feature(app.py): added some routes
    added some routes

--- src/app.py
+++ src/app.py
@@ -3,5 +3,22 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "<h1>hello world!</h1>"
+
+@app.route('/blog')
+def blog():
+    return "<h1>this is a Blog!</h1>"
+
+@app.route('/blog/<blog_id>')
+def blogpost(blog_id):
+    return "this is a Blog number " + str(blog_id)
+
+@app.route('/about')
+def about():
+    return "<h1>Know more about us</h1>"
+
+
 if '__name__' == '__main__':
     app.run()
